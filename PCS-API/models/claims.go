9eb2d251b6ba1be5d3fcd3aa7fc76e7600d524cc
Hugo <66835496+LaCapitainerie@users.noreply.github.com>
2024-07-06 14:28:01 +0200

    Delete PCS-API directory

--- PCS-API/models/claims.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/dgrijalva/jwt-go"
-
-// Claims est la structure spécifiant les données utilisées pour les tokens de connexion
-type Claims struct {
-	IdUser string `json:"idUser"`
-	jwt.StandardClaims
-}
--- PCS-API/models/reservationDTO.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type ReservationDTO struct {
-	Reservation
-	Bill    Bill         `json:"bill"`
-	Service []ServiceDTO `json:"service"`
-}
